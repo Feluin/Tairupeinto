12ae7fe6581c320f52d226762050afdae5e5fd94
Feluin <pascal.roecker.1997@gmail.com>
2020-04-18 18:35:36 +0200

    files changed

---
+++ src/main/java/sample/logicalAlgorithm/LineCombination.java
@@ -0,0 +1,16 @@
+package sample.logicalAlgorithm;
+
+import structure.Region;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineCombination {
+
+    private List<RegionIdColor> regionColorPairList=new ArrayList<>();
+
+    public LineCombination(List<RegionIdColor> regionIdColors) {
+        this.regionColorPairList=regionIdColors;
+
+    }
+}
